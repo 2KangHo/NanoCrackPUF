c3d0676eea236edfcc6a021aa321e174e63db7e5
2KangHo <ho7719@gmail.com>
2018-10-19 21:50:50 +0900

    Rename & Update code file

--- Test.py
+++ Test.py
@@ -2,17 +2,25 @@ from scipy import ndimage
 import numpy as np
 import cv2
 
-def ORB():
-    img1 = cv2.imread('img/1-1.tif', cv2.IMREAD_GRAYSCALE)
+def Test():
+    img1 = cv2.imread('img/1-1.tif')
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     #img2 = cv2.imread('img/1-2.tif')
     kpimg1 = None
     #kpimg2 = None
 
-    filtered_img1 = ndimage.gaussian_laplace(img1, sigma=2)
-    filtered_img1 = cv2.Canny(filtered_img1, 180, 200, apertureSize=3)
+    filtered_img1 = ndimage.gaussian_laplace(gray, sigma=2)
+    filtered_img1 = cv2.Canny(filtered_img1, 350, 360, apertureSize=5)
     #filtered_img2 = ndimage.gaussian_laplace(img2, sigma=1)
 
-    #lines1 = cv2.HoughLines(filtered_img1, 1, np.pi/180, threshold=140)
+    lines1 = cv2.HoughLinesP(filtered_img1, 1, np.pi/360, 70, 300, 9)
+
+    for i in range(len(lines1)):
+        for x1,y1,x2,y2 in lines1[i]:
+            cv2.line(img1, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imshow('dd', img1)
+    cv2.imshow('filter1', filtered_img1)
 
 
 
@@ -23,13 +31,13 @@ def ORB():
     kpimg1 = cv2.drawKeypoints(filtered_img1, kp1, kpimg1, (0, 0, 255), flags=0)
     #kpimg2 = cv2.drawKeypoints(filtered_img2, kp2, kpimg2, (0, 0, 255), flags=0)
 
-    cv2.imshow('filter1', filtered_img1)
-    cv2.imshow('ORB1', kpimg1)
+    #cv2.imshow('filter1', filtered_img1)
+    #cv2.imshow('ORB1', kpimg1)
     #cv2.imshow('ORB2', kpimg2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-ORB()
+Test()
 
 #TODO
 # 여러 이미지를 불러들인다.
