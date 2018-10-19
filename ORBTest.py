1cabdbaedbd88a31ce7705c3b717b48b25a87857
2KangHo <ho7719@gmail.com>
2018-10-19 19:00:48 +0900

    Update ORBTest.py

--- ORBTest.py
+++ ORBTest.py
@@ -1,23 +1,31 @@
+from scipy import ndimage
 import numpy as np
 import cv2
 
 def ORB():
-    img1 = cv2.imread('img/1-1.jpg')
-    img2 = cv2.imread('img/1-1_a.jpg')
+    img1 = cv2.imread('img/1-1.tif', cv2.IMREAD_GRAYSCALE)
+    #img2 = cv2.imread('img/1-2.tif')
     kpimg1 = None
-    kpimg2 = None
+    #kpimg2 = None
 
-    #orb = cv2.ORB_create(nfeatures=10000, scaleFactor=2, scoreType=cv2.ORB_FAST_SCORE)
-    #orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
-    orb = cv2.ORB_create(nfeatures=10000)
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    kp2, des2 = orb.detectAndCompute(img2, None)
+    filtered_img1 = ndimage.gaussian_laplace(img1, sigma=2)
+    filtered_img1 = cv2.Canny(filtered_img1, 180, 200, apertureSize=3)
+    #filtered_img2 = ndimage.gaussian_laplace(img2, sigma=1)
 
-    kpimg1 = cv2.drawKeypoints(img1, kp1, kpimg1, (0, 0, 255), flags=0)
-    kpimg2 = cv2.drawKeypoints(img2, kp2, kpimg2, (0, 0, 255), flags=0)
+    #lines1 = cv2.HoughLines(filtered_img1, 1, np.pi/180, threshold=140)
 
+
+
+    orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
+    #orb = cv2.ORB_create(nfeatures=10000)
+    kp1, des1 = orb.detectAndCompute(filtered_img1, None)
+
+    kpimg1 = cv2.drawKeypoints(filtered_img1, kp1, kpimg1, (0, 0, 255), flags=0)
+    #kpimg2 = cv2.drawKeypoints(filtered_img2, kp2, kpimg2, (0, 0, 255), flags=0)
+
+    cv2.imshow('filter1', filtered_img1)
     cv2.imshow('ORB1', kpimg1)
-    cv2.imshow('ORB2', kpimg2)
+    #cv2.imshow('ORB2', kpimg2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
